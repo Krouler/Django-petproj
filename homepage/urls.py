fcfda11776485302850c276f9d770bba7e3f669f
Andrey Lesorukov <C:\Users\lesor\AppData\Roaming\The Bat!>
2022-10-25 18:29:01 +0300

    templatesdone

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_list, name='homepage_list')
+    path('', views.homepage_list, name='homepage_list'),
+    path('biography/', views.biography, name='biography'),
+    path('skills/', views.skills, name='skills')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def homepage_list(request, *args, **kwards):
-    return HttpResponse('<h1> Hello, my name is Lesor </h1>'
-                        '<br>'
-                        '<h3> This is my first page in djangoproject, glad to see you </h3>'
-                        '<br>'
-                        '<a href="admin/"> If you want, you may try to get to admin-panel</a>'
-                        )
+
+
+def homepage_list(request, *args):
+    return render(request, 'homepage/homepage_list.html', {})
+
+
+def biography(request, *args):
+    return render(request, 'homepage/biography.html')
+
+
+def skills(request, *args):
+    return render(request, 'homepage/skills.html')
