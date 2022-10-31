b7aa73a563f0f51b9de3712d7c04273391d75d67
Andrey Lesorukov <C:\Users\lesor\AppData\Roaming\The Bat!>
2022-10-31 16:02:10 +0300

    some_few_more_templates

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.homepage_list, name='homepage_list'),
     path('biography/', views.biography, name='biography'),
-    path('skills/', views.skills, name='skills')
+    path('skills/', views.skills, name='skills'),
+    path('about/', views.aboutwww, name='about'),
+    path('contacts/',views.contacts,name='contacts'),
+path('categories/',views.categories,name='categories')
 ]
