29bcaee43f47bf6c2095ec8485bc829d97d78968
Andrey Lesorukov <C:\Users\lesor\AppData\Roaming\The Bat!>
2022-10-31 14:54:35 +0300

    updated_rule_for_429

--- homepage/middleware/IPWork.py
+++ homepage/middleware/IPWork.py
@@ -3,17 +3,25 @@ from django.shortcuts import render
 
 class Zadderjka:
     def __init__(self, get_response):
+        self.bannedaddrcoll429= {}
         self.addrcollection={}
         self.get_response = get_response
 
 
     def __call__(self, request):
         remip = request.META.get('REMOTE_ADDR')
+        if remip in self.bannedaddrcoll429.keys() and self.bannedaddrcoll429[remip]>time.time_ns():
+            return render(request, 'homepage/429.html', status=429)
+        else:
+            try:
+                del self.bannedaddrcoll429[remip]
+            except KeyError:
+                pass
         if remip in self.addrcollection.keys():
             self.addrcollection[str(remip)][0] += 1
             if self.addrcollection[str(remip)][0] % 5 == 0:
                 if time.time_ns() - self.addrcollection[str(remip)][1] < 1000000000:
-                    return render(request, 'homepage/429.html', status=429)
+                    self.bannedaddrcoll429[remip] = time.time_ns()+10000000000
                 self.addrcollection[str(remip)][1] = time.time_ns()
         else:
             self.addrcollection[str(remip)] = [1, time.time_ns()]
