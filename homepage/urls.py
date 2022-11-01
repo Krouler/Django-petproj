b0daf94931653493190782e089426b5c252167aa
Andrey Lesorukov <C:\Users\lesor\AppData\Roaming\The Bat!>
2022-11-01 12:36:18 +0300

    slightly_changes_for_scaling

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_list, name='homepage_list'),
-    path('biography/', views.biography, name='biography'),
-    path('skills/', views.skills, name='skills'),
-    path('about/', views.aboutwww, name='about'),
-    path('contacts/',views.contacts,name='contacts'),
-path('categories/',views.categories,name='categories')
+    path('', views.start, name='start'),
+    path('homepage/', views.homepage_list, name='homepage_list'),
+    path('homepage/biography/', views.biography, name='biography'),
+    path('homepage/skills/', views.skills, name='skills'),
+    path('homepage/about/', views.aboutwww, name='about'),
+    path('homepage/contacts/',views.contacts,name='contacts'),
+    path('homepage/categories/',views.categories,name='categories'),
 ]
