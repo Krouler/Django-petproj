c94cdb12f64c940280443db73910f8a269d90f21
Andrey Lesorukov <C:\Users\lesor\AppData\Roaming\The Bat!>
2022-11-08 17:36:57 +0300

    smth

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from dashboard.models import Dashes
 
 # Create your views here.
 def home(request, *args):
     return render(request, 'home/home.html', {})
+
+def dashboards(request, *args):
+    dash = Dashes.objects.all()
+    return render(request, 'dashboard/dashboard.html', {'dashboards': dash})
